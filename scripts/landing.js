3ca4718a3c176cd9f0eadc06c2d492c74d867021
SaintClair33 <ceo.wonu@gmail.com>
2015-11-14 18:09:40 -0500

    Added JS callback

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
- var animatePoints = function() 
- {
-     var points = document.getElementsByClassName('point');
-     
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points)
+{
      var revealPoint = function (index)
      {
          points[index].style.opacity = 1;
@@ -19,3 +19,21 @@
  
  
  };
+
+window.onload = function()
+{
+    // Automagically animate points on a tall screen where scrolling is not needed
+    if (window.innerHeight > 950)
+    {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) { 
+        if (document.body.scrollTop >= scrollDistance)
+        {
+            animatePoints(pointsArray);   
+        }});
+        
+        
+}
