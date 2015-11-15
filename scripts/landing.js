251c629466666d4db770ba6531fd4fef88b0e158
SaintClair33 <ceo.wonu@gmail.com>
2015-11-14 20:56:48 -0500

    added window load and listner functions

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
- var animatePoints = function() 
- {
-     var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+ 
+var animatePoints = function(points) { 
      
      var revealPoint = function (index)
      {
@@ -9,13 +9,26 @@
          points[index].style.msTransform = "scaleX(1) translateY(0)";
          points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
          
-     }
+     };
  
      
      for (var i = 0; i < points.length; i++)
      {
        revealPoint(i);   
      }
- 
- 
- };
+};
+
+window.onload = function() {
+        // Automatically animate the points on a tall screen where scrolling   can't trigger the animation
+    if (window.innerHeight > 950) 
+        {
+            animatePoints(pointsArray);
+        }
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+     window.addEventListener("scroll", function(event) {
+     if (document.body.scrollTop >= scrollDistance) 
+     {
+        animatePoints(pointsArray);   
+     }});
+ }
