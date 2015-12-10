b0cc256bda1c684dc9a8b9fdac7234732e8155b7
SaintClair33 <ceo.wonu@gmail.com>
2015-12-09 19:37:43 -0500

    jquery landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,34 +1,51 @@
-var pointsArray = document.getElementsByClassName('point');
+//var pointsArray = document.getElementsByClassName('point'); vanilla js
  
-var animatePoints = function(points) { 
+//var animatePoints = function(points) { vanilla js
+var animatePoints = function() { //jquery route
      
-     var revealPoint = function (index)
+     //var revealPoint = function (index)
+       var revealPoint = function()
      {
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+         $(this).css({opacity: 1, transform: 'scaleX(1) translateY(0)'});//jquery route
+     };
+         //points[index].style.opacity = 1;
+         //points[index].style.transform = "scaleX(1) translateY(0)";
+         //points[index].style.msTransform = "scaleX(1) translateY(0)";
+         //points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    
+    //for (var i = 0; i < points.length; i++)
+     //{
+       //revealPoint(i);   
+     //}
+        $.each($('.point'), revealPoint);
+
          
      };
  
      
-     for (var i = 0; i < points.length; i++)
-     {
-       revealPoint(i);   
-     }
-};
+     
+//window.onload = function() vanilla js route      
+$(window).load = (function() {//jquery 
 
-window.onload = function() {
         // Automatically animate the points on a tall screen where scrolling   can't trigger the animation
-    if (window.innerHeight > 950) 
+    //if (window.innerHeight > 950) vanilla js route
+    if ($(window).height() > 950) //jquery
         {
-            animatePoints(pointsArray);
+            //animatePoints(pointsArray);//vanilla js
+            animatePoints();
         }
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-     window.addEventListener("scroll", function(event) {
-     if (document.body.scrollTop >= scrollDistance) 
+     //var sellingPoints = document.getElementsByClassName('selling-points')[0]; vanilla js
+     //var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200; vanilla js
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+     //window.addEventListener("scroll", function(event) {
+    $(window).scroll(function(event) {
+     //if (document.body.scrollTop >= scrollDistance) 
+        if($(window).scrollTop() >= scrollDistance)
      {
-        animatePoints(pointsArray);   
-     }});
- }
+        //animatePoints(pointsArray);
+         animatePoints();
+     }
+    });
+});
+
+ 
