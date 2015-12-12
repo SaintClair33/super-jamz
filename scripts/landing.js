1fa12ab1e0b04cc0607525d68f2047f9104e6c63
SaintClair33 <ceo.wonu@gmail.com>
2015-12-12 08:41:55 -0500

    refactored to use doc ready

--- scripts/landing.js
+++ scripts/landing.js
@@ -8,44 +8,32 @@ var animatePoints = function() { //jquery route
      {
          $(this).css({opacity: 1, transform: 'scaleX(1) translateY(0)'});//jquery route
      };
-         //points[index].style.opacity = 1;
-         //points[index].style.transform = "scaleX(1) translateY(0)";
-         //points[index].style.msTransform = "scaleX(1) translateY(0)";
-         //points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    
-    //for (var i = 0; i < points.length; i++)
-     //{
-       //revealPoint(i);   
-     //}
+         
         $.each($('.point'), revealPoint);
 
          
      };
  
-     
-     
-//window.onload = function() vanilla js route      
-$(window).load = (function() {//jquery 
-
-        // Automatically animate the points on a tall screen where scrolling   can't trigger the animation
-    //if (window.innerHeight > 950) vanilla js route
-    if ($(window).height() > 950) //jquery
+$(document).ready(function(){
+    
+        if ($(window).height() > 950) //jquery
         {
-            //animatePoints(pointsArray);//vanilla js
+            
             animatePoints();
         }
-     //var sellingPoints = document.getElementsByClassName('selling-points')[0]; vanilla js
-     //var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200; vanilla js
-    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+        var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
      //window.addEventListener("scroll", function(event) {
     $(window).scroll(function(event) {
      //if (document.body.scrollTop >= scrollDistance) 
         if($(window).scrollTop() >= scrollDistance)
      {
         //animatePoints(pointsArray);
+         console.log("test");
          animatePoints();
      }
     });
-});
 
- 
+})  
+     
+
